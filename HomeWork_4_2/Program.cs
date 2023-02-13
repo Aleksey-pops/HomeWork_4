89e3a45a97f0e4bfd00eaab168c73b34173e24d7
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-13 11:32:22 +0300

    Completion of the second task.

--- HomeWork_4_2/Program.cs
+++ HomeWork_4_2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine()!);
+int j = 0;
+int t = 0;
+int[] Array = new int[length];
+Random rnd = new Random(); //подключаем случайные числа
+for (int i = 0; i<length; i++)
+{
+Array[i] = rnd.Next(100,1000);
+        if (Array[i] % 2 == 0)
+        {
+            Console.WriteLine($"Порядковый номер в массиве {i}: \tЧетное число \t{Array[i]}");j++;
+            
+        }
+    else
+    {
+        Console.WriteLine($"Порядковый номер в массиве {i}: \tНечетное  число {Array[i]}");t++;
+       
+    }
+    
+}
+Console.WriteLine($"Количество четных чисел {j}");
+Console.WriteLine($"Количество нечетных чисел {t}");
---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine()!);
+
+
+int[] Array = new int[length];
+Random rnd = new Random(); //подключаем случайные числа
+for (int i=0; i<length; i++)
+{
+Array[i] = rnd.Next(100,1000);
+        if (Array[i] % 2 == 0)
+        {
+            Console.WriteLine($"Порядковый номер в массиве {i}: \tЧетное число \t{Array[i]}");
+        }
+        else
+        {
+        Console.WriteLine($"Порядковый номер в массиве {i}: \tНечетное  число {Array[i]}");
+        }     
+}
