d6d54b5e69c98e24fa7f93b97b826494838a8c98
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-13 13:05:09 +0300

    Correction of the third task.

--- HomeWork_4_3/Program.cs
+++ HomeWork_4_3/Program.cs
@@ -6,11 +6,14 @@ int j = 0;
 for (int i=0; i<123; i++)
 {
     newArray[i] = rnd.Next(1,200);
+   
     Console.WriteLine($"Число масива №{i + 1} \t{newArray[i]}  ");
+    //Console.WriteLine();
     if (newArray[i]>=10 && newArray[i]<=99)
-    {
-    //    Console.WriteLine($" {j})  ");
-    j++; 
+    {  
+        j++; 
+        Console.WriteLine($"Подсчет элементов({j})");
+        // Console.WriteLine();
     }
 }
 Console.WriteLine($"Количество элементов которые находятся в отрезке [10,99] = {j}");
