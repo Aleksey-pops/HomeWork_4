05e98fcd4250ce359366bb4dda11d61950607d95
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-13 12:38:53 +0300

    Completion of the third task.

--- HomeWork_4_3/Program.cs
+++ HomeWork_4_3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 3. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] newArray = new int[123];
+Random rnd = new Random(); //подключаем случайные числа
+int j = 0;
+for (int i=0; i<123; i++)
+{
+    newArray[i] = rnd.Next(1,200);
+    Console.WriteLine($"Число масива №{i + 1} \t{newArray[i]}  ");
+    if (newArray[i]>=10 && newArray[i]<=99)
+    {
+    //    Console.WriteLine($" {j})  ");
+    j++; 
+    }
+}
+Console.WriteLine($"Количество элементов которые находятся в отрезке [10,99] = {j}");
