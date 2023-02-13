e79eea095db8d249899ab3b280778d52ee6988e5
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-13 16:03:57 +0300

    Completion of the fourth task.

--- HomeWork_4_4/Program.cs
+++ HomeWork_4_4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+
+Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine()!);
+int j = 0;
+int t = 0;
+int sum = 0;
+int sum1 =0;
+int[] Array = new int[length];
+Random rnd = new Random();
+for (int i = 0; i<length; i++)
+{
+Array[i] = rnd.Next(1,100);
+    if(i % 2 == 0)
+        {
+            sum += Array[i];
+            Console.Write($"Порядковый номер в массиве {i},   \tЧисло масива({Array[i]})  \tСумма чисел =  {sum}");
+            j++;
+        }
+    if(i % 2 != 0)
+        {
+            sum1 += Array[i];
+            Console.WriteLine($"\tПорядковый номер в массиве {i},   \tЧисло масива({Array[i]})  \tСумма чисел \t{sum1}");
+            t++;
+        }
+}
+Console.WriteLine($"\nКоличество чисел стоящих на четной позиции ={j} \tСумма чисел стоящее на четной позиции {sum}");
+Console.WriteLine($"Количество чисел стоящих на четной позиции ={t} \tСумма чисел стоящее на нечетной позиции {sum1}");
