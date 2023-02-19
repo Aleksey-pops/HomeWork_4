a04609e138ff0707474432fcbfaaaf6fe7090ae5
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-19 22:01:32 +0300

    Find the product of pairs of numbers in a one-dimensional array.

--- HomeWork_4_5/Program.cs
+++ HomeWork_4_5/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 5. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+using System.Data;
+Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine()!);
+int[] array = new int[length];
+Random rnd = new Random(); //подключаем случайные числа
+for (int i = 0; i<length; i++)
+{
+array[i] = rnd.Next(1,10);
+Console.Write($"{array[i]}  ");
+}
+Console.WriteLine();
+for (int i = 0,j = 1; i < length/2; i++, j++)
+{
+    Console.WriteLine($"Произведение: {array[i]} * {array[length-j]} = {array[i]*array[length-j]}");
+}
