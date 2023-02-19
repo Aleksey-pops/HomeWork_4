5c95aed71b047619ed7de25413d2080f705f39d0
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-19 23:21:34 +0300

    In the Specified Array of Real Numbers, find the difference between the maximum and minimum element.

--- HomeWork_4_1/Program.cs
+++ HomeWork_4_1/Program.cs
@@ -1,15 +1,8 @@
 ﻿// 1 .Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
-// int[] array = new int[8];
-// for (int i = 0; i < array.Length; i++)
-// {
-//     array[i] = new Random().Next(0, 2);
-//     Console.Write($"\t {array[i]}");
-// }
-
 int[] newArray = new int[8];
 for (int i = 0; i < newArray.Length; i++)
 {
     newArray[i] = new Random().Next(0, 2);
-    Console.WriteLine($"Порядковый номкр № {i + 1}  \t Элементы масива из 8 цифр от 0 до 1 = {newArray[i]}");
+    Console.WriteLine($"Порядковый номер № {i + 1}  \t Элементы масива из 8 цифр от 0 до 1 = {newArray[i]}");
 }
--- HomeWork_4_6/Program.cs
+++ HomeWork_4_6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 6. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+Console.Write("Введите размер массива: ");
+int number = int.Parse(Console.ReadLine()!);
+double max = 0;
+double min = 200;
+double[] newArray = new double[number];
+for (int i = 0; i < newArray.Length; i++)
+{
+    newArray[i] = new Random().Next(100, 200)* 0.01;
+    Console.WriteLine($"Порядковый номер № {i + 1}  \t Элементы масива = {newArray[i]}");
+    if(newArray[i] > max)
+       max = newArray[i];
+    if(newArray[i]< min)
+        min = newArray[i]; 
+}
+Console.WriteLine($"Максимальное число = {max}");
+Console.WriteLine($"Минимальное число = {min}");
+Console.WriteLine($"Разница между максимальным и минимальным числом = {max - min}");
